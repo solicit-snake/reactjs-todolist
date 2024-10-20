c92ce5644542816d36094136226e7a21d099982c
solicit-snake <kyle.vinc11@gmail.com>
2024-10-20 11:04:06 +0800

    Added delete todo functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react"
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
  
 function App() {
 
+  const [todos, setTodos] = useState([
+    'Go to the gym',
+    'Eat more fruit and vege',
+    'Pick up the kids from school'
+  ])
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    setTodos(newTodoList)
+  }
+
+  function handleDeleteTodo(index){
+    const newTodoList = todos.filter( (todo, todoIndex) => {
+      return todoIndex !== index
+    } )
+    setTodos(newTodoList)
+  }
+
+  function handleEditTodo(index){
+
+  }
+
   return (
     <>
-      <TodoInput/>
-      <TodoList/>
+      <TodoInput handleAddTodos = {handleAddTodos}/>
+      <TodoList todos = { todos }   handleDeleteTodo = {handleDeleteTodo}/>
     </>
   )
 }
